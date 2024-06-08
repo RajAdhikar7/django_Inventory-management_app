fc741c11cbe663af3aafd04f92a855dda4e7cd19
RajAdhikar7 <adhikarraj77@gmail.com>
2024-06-08 15:22:52 +0530

    added test cases using relationships , added validators.py , utils.py added form in recipe app , updated model methods in recipe created views in recipe app , updated admin.py for recipe

--- trydjango/recipes/models.py
+++ trydjango/recipes/models.py
@@ -1,5 +1,8 @@
 from django.conf import settings
 from django.db import models
+from .utils import number_str_to_float
+import pint
+from.validators import validate_unit_of_measure 
 
 # Create your models here.
 class Recipe(models.Model):
@@ -11,13 +14,47 @@ class Recipe(models.Model):
     updated = models.DateTimeField(auto_now=True) 
     active = models.BooleanField(default=True)
 
+    def get_absolute_url(self):
+        return "/pantry/recipes/"
+
+
 class RecipeIngredient(models.Model):
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
     name = models.CharField(max_length=220)
     description = models.TextField(blank=True, null=True)
-    quanity = models.CharField(max_length=50)  # 1 1/4
-    unit = models.CharField(max_length=50) # pounds, lbs, oz, gram, etc
+    quantity = models.CharField(max_length=50)  # 1 1/4
+    quantity_as_float = models.FloatField(blank=True, null=True)
+    unit = models.CharField(max_length=50, validators=[validate_unit_of_measure])     
     directions = models.TextField(blank=True, null=True)
     timestamp = models.DateTimeField(auto_now_add=True) 
     updated = models.DateTimeField(auto_now=True) 
     active = models.BooleanField(default=True)
+
+    def get_absolute_url(self):
+        return self.recipe.get_absolute_url()
+    
+    def convert_to_system(self, system="mks"):
+        if self.quantity_as_float is None:
+            return None
+        ureg = pint.UnitRegistry(system=system)
+        measurement = self.quantity_as_float * ureg[self.unit]
+        return measurement #.to_base_units()
+
+    def as_mks(self):
+        # meter, kilogram, second
+        measurement = self.convert_to_system(system='mks')
+        return measurement.to_base_units()
+
+    def as_imperial(self):
+        # miles, pounds, seconds
+        measurement = self.convert_to_system(system='imperial')
+        return measurement.to_base_units()
+    
+    def save(self, *args, **kwargs):
+        qty = self.quantity
+        qty_as_float, qty_as_float_success = number_str_to_float(qty)
+        if qty_as_float_success:
+            self.quantity_as_float = qty_as_float
+        else:
+            self.quantity_as_float = None
+        super().save(*args, **kwargs)
